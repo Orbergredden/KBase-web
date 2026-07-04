6a722756cc2d3e642b0597463b2b78a5027affa2
Igor Makarevich <mcduke_pl@i.ua>
2026-07-04 17:23:36 +0300

    Перестворив фронтенд з версією 21.

--- kbase-frontend/src/app/app.spec.ts
+++ kbase-frontend/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, KBase_frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, kbase-frontend');
   });
 });
--- kbase-frontend/src/app/app.ts
+++ kbase-frontend/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('KBase_frontend');
+  protected readonly title = signal('kbase-frontend');
 }
